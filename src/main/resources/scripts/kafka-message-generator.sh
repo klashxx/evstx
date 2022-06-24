824e100fc46fa15f80d359722408c07037d878ae
klashxx <klashxx@gmail.com>
2022-06-24 10:49:25 +0200

    basic struct

--- src/main/resources/scripts/kafka-message-generator.sh
+++ src/main/resources/scripts/kafka-message-generator.sh
@@ -5,7 +5,7 @@
 while true; do
   id=$(cat /proc/sys/kernel/random/uuid|tr -d -)
   timestamp=$(date +%s)
-  echo "{\"id\": \"$id\", \"timestamp\": \"$timestamp\"}" > /tmp/tmp.json
+  echo "{\"id\": \"$id\", \"timestamp\": $timestamp}" > /tmp/tmp.json
   kafka-console-producer -bootstrap-server broker:9092 --topic evstx < /tmp/tmp.json
   sleep 10
 done
--- src/main/scala/com/klashxx/github/st/Consumer.scala
+++ src/main/scala/com/klashxx/github/st/Consumer.scala
@@ -1,23 +1,35 @@
 package com.klashxx.github.st
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, from_json}
 import org.apache.spark.sql.streaming.Trigger
+import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
 
 object Consumer extends App {
   println("Consuming ...")
 
-  val spark = SparkSession.builder.appName("evstx").master("local").getOrCreate
+  val spark = SparkSession.builder
+    .appName("evstx")
+    .master("local")
+    .getOrCreate
 
   val df = spark.readStream
     .format("kafka")
     .option("kafka.bootstrap.servers", "localhost:9092")
-    .option("subscribe", "quickstart")
-    .option("startingOffsets", "earliest")
+    .option("subscribe", "evstx")
+    .option("startingOffsets", "latest")
     .load()
 
   df.printSchema()
 
-  df.writeStream
+  val schema = new StructType()
+    .add("id", StringType)
+    .add("timestamp", IntegerType)
+
+  df.selectExpr("CAST(value AS STRING)")
+    .select(from_json(col("value"), schema).as("data"))
+    .select("data.*")
+    .writeStream
     .format("console")
     .outputMode("append")
     .trigger(Trigger.ProcessingTime("20 seconds"))
