c23d27690cf258fa78dd330f4224caeec49829d4
klashxx <klashxx@gmail.com>
2022-06-24 10:32:29 +0200

    basic message generator

---
+++ src/main/resources/scripts/kafka-message-generator.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# MUST RUN IN THE BROKER CONTAINER
+
+while true; do
+  id=$(cat /proc/sys/kernel/random/uuid|tr -d -)
+  timestamp=$(date +%s)
+  echo "{\"id\": \"$id\", \"timestamp\": \"$timestamp\"}" > /tmp/tmp.json
+  kafka-console-producer -bootstrap-server broker:9092 --topic evstx < /tmp/tmp.json
+  sleep 10
+done
