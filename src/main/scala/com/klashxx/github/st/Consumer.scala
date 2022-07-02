51810153e2f84b45960718fd459730d00e97edf9
klashxx <klashxx@gmail.com>
2022-07-02 13:37:10 +0200

    casting

--- src/main/scala/com/klashxx/github/st/Consumer.scala
+++ src/main/scala/com/klashxx/github/st/Consumer.scala
@@ -2,9 +2,10 @@ package com.klashxx.github.st
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.catalyst.ScalaReflection
-import org.apache.spark.sql.functions.{col, from_json}
+import org.apache.spark.sql.functions.{col, from_json, sum, window}
 import org.apache.spark.sql.streaming.Trigger
 import org.apache.spark.sql.types.StructType
+
 
 
 object Consumer extends App {
@@ -26,11 +27,16 @@ object Consumer extends App {
 
   df.printSchema()
 
+  import spark.implicits._
+
+
   val schema = ScalaReflection.schemaFor[Message].dataType.asInstanceOf[StructType]
 
   df.selectExpr("CAST(value AS STRING)")
     .select(from_json(col("value"), schema).as("data"))
     .select("data.*")
+    .selectExpr("id as id" , "CAST(timestamp AS TIMESTAMP)", "flag as flag")
+    .as[Message]
     .writeStream
     .format("console")
     .outputMode("append")
